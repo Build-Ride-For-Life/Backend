c22c645b47f8673a08b8bc8b204230e1d6daec33
avi <avikan1087@gmail.com>
2020-02-01 00:08:57 -0500

    drivers,reviews and users models

---
+++ users/users-model.js
@@ -0,0 +1,46 @@
+const db = require('../data/dbConfig.js');
+
+module.exports = {
+    findBy,
+    findById,
+    findReviews,
+    add,
+    update,
+    remove
+}
+
+function findBy(filter) {
+    return db('users').where(filter)
+}
+
+function findById(id) {
+    return db('users').where({ id }).first();
+}
+
+function findReviews(id) {
+    return db('reviews').where({ user_id: id })
+}
+
+async function add(user) {
+    return db('users')
+    .returning(['id', 'users_name'])
+    .insert(user, 'id')
+    .then(response => {
+        return findById(response[0])
+    })
+}
+
+async function update(changes, id) {
+    return db('users')
+    .where({ id })
+    .update({ ...changes })
+    .then(respon => {
+        return findById(id)
+    })
+}
+
+async function remove(id) {
+    return db('users')
+    .where({ id })
+    .del()
+}
